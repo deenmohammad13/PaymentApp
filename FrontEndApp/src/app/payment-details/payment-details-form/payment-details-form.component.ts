9aa07a91e87debfb75bd9c213792d25c7d51d63f
Deen Mohammad <deen9721@gmail.com>
2025-04-26 22:06:13 +0600

    Post request

--- FrontEndApp/src/app/payment-details/payment-details-form/payment-details-form.component.ts
+++ FrontEndApp/src/app/payment-details/payment-details-form/payment-details-form.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { PaymentDetailService } from '../../shared/payment-detail.service';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
+import { PaymentDetail } from '../../shared/payment-detail.model';
 
 @Component({
   selector: 'app-payment-details-form',
@@ -11,4 +12,14 @@ import { FormsModule } from '@angular/forms';
 export class PaymentDetailsFormComponent {
 
    constructor(public service : PaymentDetailService){}
+  
+   onSubmit(form:NgForm){
+    this.service.postPaymentDetail()
+    .subscribe({
+      next : res => {this.service.list = res as PaymentDetail[]
+        this.service.resetForm(form)
+      },
+      error : err => {console.log(err);}
+    })
+   }
 }
--- FrontEndApp/src/app/payment-details/payment-details.component.ts
+++ FrontEndApp/src/app/payment-details/payment-details.component.ts
@@ -2,6 +2,7 @@ import { Component,OnInit } from '@angular/core';
 import { PaymentDetailsFormComponent } from './payment-details-form/payment-details-form.component';
 import { PaymentDetailService } from '../shared/payment-detail.service';
 import { CommonModule } from '@angular/common';
+import { PaymentDetail } from '../shared/payment-detail.model';
 
 @Component({
   selector: 'app-payment-details',
@@ -14,5 +15,10 @@ export class PaymentDetailsComponent implements OnInit {
   constructor(public service : PaymentDetailService){}
   ngOnInit(): void {
     this.service.refreshList();
+    this.service.getPaymentDetail()
+    .subscribe({
+      next: res => {this.service.list = res as PaymentDetail[]},
+      error: err => {console.log(err);}
+    })
   }
 }
